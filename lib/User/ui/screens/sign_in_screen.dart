34f5a43a90ee6bcc57526aa4efc978555325a079
Germán Arevalo <germanarevalojerez@gmail.com>
2019-07-19 10:45:48 -0500

    pantalla de Login

---
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/widgets/gradient_back.dart';
+import 'package:platzi_trips_app/widgets/button_green.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return _SignInScreen();
+  }
+}
+
+class _SignInScreen extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return signInGoogleUI();
+  }
+
+  Widget signInGoogleUI() {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          GradientBack("", null),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome \n This is your travel app",
+                style: TextStyle(
+                    fontSize: 37.0,
+                    fontFamily: "Lato",
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              ButtonGreen(
+                text: "Login with Gmail",
+                onPressed: () {},
+                width: 300.0,
+                height: 50.0,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
